00061e684092bfe6d99a9eae2b85c668d2b33a53
muhammad-usama-ad <muhammad.usama@aerodyne.group>
2025-01-22 15:57:18 +0500

    update fixations

---
+++ libs/database/abstract.schema.ts
@@ -0,0 +1,9 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { SchemaTypes, Types } from 'mongoose';
+
+@Schema()
+export class AbstractDocument {
+    //@ts-ignore
+  @Prop({ type: SchemaTypes.ObjectId })
+  _id: Types.ObjectId;
+}
